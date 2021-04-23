142413a416194a8ac41c999a4554cc49643b4c73
Alina <77508351+Alina1103master@users.noreply.github.com>
2021-04-23 10:34:22 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int starting;
+        int payment = 1300;
+        int bonus;
+        if (payment <= 1000) {
+            payment = 1000;
+                    bonus=1;
+            } else {
+            bonus = payment / 100;
+        }
+        int statrting = 400;
+        payment = 1300;
+        System.out.println( bonus );
+        
+    }
+}
